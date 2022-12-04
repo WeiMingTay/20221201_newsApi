86cb2d8cd270f0620706242486b7a269f89f15bc
Wei Ming Tay <ming23.tay@gmail.com>
2022-12-04 01:03:42 +0100

    v01

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+let input = document.querySelector("suchen");
+fetch(
+	"https://newsapi.org/v2/everything?q=tesla&from=2022-11-15&sortBy=publishedAt&apiKey=61a51e6230e145a2b1bda71a11df1048"
+)
+	.then((response) => response.json())
+	.then((data) => {
+		console.log(data.articles[2]);
+
+		for (let e = 0; e < data.articles.length; e++) {
+			let ausgabe = document.querySelector(".newsAusgabe");
+			ausgabe.class = "ausgabe";
+
+			let divArticle = document.createElement("div");
+			divArticle.class = "article";
+			ausgabe.appendChild(divArticle);
+
+			let img = document.createElement("img");
+			img.src = data.articles[e].urlToImage;
+			img.alt = data.articles[e].author;
+			divArticle.appendChild(img);
+
+			let divInhalt = document.createElement("div");
+			divInhalt.class = "inhalt";
+			divArticle.appendChild(divInhalt);
+
+			let authorP = document.createElement("p");
+			authorP.innerHTML = data.articles[e].author;
+			divInhalt.appendChild(authorP);
+
+			let h2 = document.createElement("h2");
+			h2.innerHTML = data.articles[e].title;
+			divInhalt.appendChild(h2);
+
+			let p = document.createElement("p");
+			p.innerHTML = data.articles[e].description;
+			divInhalt.appendChild(p);
+
+			let date = document.createElement("date");
+			date.innerHTML = new Date(
+				data.articles[e].publishedAt
+			).toLocaleDateString();
+			divInhalt.appendChild(date);
+
+			let a = document.createElement("a");
+			a.innerText = "Read More";
+			a.target = "_blank";
+			a.href = data.articles[e].url;
+			divInhalt.appendChild(a);
+
+			/* let artikelLink = document.querySelector(".article");
+			artikelLink.addEventListener("click", () => {
+				window.open(data.articles[e].url);
+			}); */
+		}
+	});
---
+++ readme.md
@@ -0,0 +1,22 @@
+Link zur Seite: https://weimingtay.github.io/20221201_newsApi
+
+# Aufgabenstellung
+
+
+Für dieses Projekt verwenden wir die APIs News, um eine Nachrichten-Website zu erstellen.
+https://newsapi.org/
+
+Um diese API nutzen zu können und den API-Key zu erhalten, müssen wir uns registrieren. 
+
+Nachdem du die Daten von der API erhalten hast, kannst du sie verwenden, um die Nachrichten in einem ansprechenden Design anzuzeigen.
+
+Bonus: 
+Anzeige nach Land
+Über Schlüsselwort suchen
+
+WICHTIG:
+Wenn du eine Fehlermeldung mit “corsNotAllowed” bekommst:
+Nutze kein https, sondern http!
+
+## Ergebnisvorschau
+![Alt text](assets/img/Screenshot%202022-12-01%20201306.png)
