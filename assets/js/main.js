2c5c89456ed85ad14222059677b3ec532532dddb
Wei Ming Tay <ming23.tay@gmail.com>
2022-12-04 22:47:03 +0100

    url

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,72 @@
-let input = document.querySelector("suchen");
-fetch(
-	"https://newsapi.org/v2/everything?q=movies&from=2022-12-01&sortBy=publishedAt&apiKey=61a51e6230e145a2b1bda71a11df1048"
-)
-	.then((response) => response.json())
-	.then((data) => {
-		console.log(data.articles[2]);
+let buttonSuche = document.querySelector("#buttonSuche");
+let datum = document.querySelector("#datum");
 
-		for (let e = 0; e < 30; e++) { //data.articles.length
-			let ausgabe = document.querySelector(".newsAusgabe");
-			ausgabe.class = "ausgabe";
+datum.addEventListener("input", () => {
+	console.log(datum.value);
+	console.log(new Date());
+	buttonSuche.addEventListener("click", () => {
+		let input = document.querySelector("#suchen");
+		let inputLabel = document.querySelector("#labelSuche");
 
-			let divArticle = document.createElement("div");
-			divArticle.class = "article";
-			ausgabe.appendChild(divArticle);
+		inputLabel.innerHTML = input.value;
+		console.log(input.value);
 
-			let img = document.createElement("img");
-			img.src = data.articles[e].urlToImage;
-			img.alt = data.articles[e].author;
-			divArticle.appendChild(img);
+		let url = 
+		`http://newsapi.org/v2/everything?q=${input.value}&from=${datum.value}&sortBy=publishedAt&apiKey=61a51e6230e145a2b1bda71a11df1048`;
 
-			let divInhalt = document.createElement("div");
-			divInhalt.class = "inhalt";
-			divArticle.appendChild(divInhalt);
+		console.log(url);
 
-			let authorP = document.createElement("p");
-			authorP.innerHTML = data.articles[e].author;
-			divInhalt.appendChild(authorP);
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => {
+				for (let e = 0; e < 30; e++) {
+					let ausgabe = document.querySelector(".newsAusgabe");
+					ausgabe.class = "ausgabe";
 
-			let h2 = document.createElement("h2");
-			h2.innerHTML = data.articles[e].title;
-			divInhalt.appendChild(h2);
+					let divArticle = document.createElement("div");
+					divArticle.class = "article";
+					ausgabe.appendChild(divArticle);
 
-			let p = document.createElement("p");
-			p.innerHTML = data.articles[e].description;
-			divInhalt.appendChild(p);
+					let img = document.createElement("img");
+					img.src = data.articles[e].urlToImage;
+					img.alt = data.articles[e].author;
+					divArticle.appendChild(img);
 
-			let date = document.createElement("date");
-			date.innerHTML = new Date(
-				data.articles[e].publishedAt
-			).toLocaleDateString();
-			divInhalt.appendChild(date);
+					let divInhalt = document.createElement("div");
+					divInhalt.class = "inhalt";
+					divArticle.appendChild(divInhalt);
 
-			let a = document.createElement("a");
-			a.innerText = "Read More";
-			a.target = "_blank";
-			a.href = data.articles[e].url;
-			divInhalt.appendChild(a);
+					let authorP = document.createElement("p");
+					authorP.innerHTML = data.articles[e].author;
+					divInhalt.appendChild(authorP);
 
-			/* let artikelLink = document.querySelector(".article");
+					let h2 = document.createElement("h2");
+					h2.innerHTML = data.articles[e].title;
+					divInhalt.appendChild(h2);
+
+					let p = document.createElement("p");
+					p.innerHTML = data.articles[e].description;
+					divInhalt.appendChild(p);
+
+					let date = document.createElement("date");
+					date.innerHTML = new Date(
+						data.articles[e].publishedAt
+					).toLocaleDateString();
+					divInhalt.appendChild(date);
+
+					let a = document.createElement("a");
+					a.innerText = "Read More";
+					a.target = "_blank";
+					a.href = data.articles[e].url;
+					divInhalt.appendChild(a);
+
+					/* let artikelLink = document.querySelector(".article");
 			artikelLink.addEventListener("click", () => {
 				window.open(data.articles[e].url);
 			}); */
-		}
+				}
+			});
+
+		input.value = "";
 	});
+});
